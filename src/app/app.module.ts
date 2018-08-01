cad3aba4f27e8d1042946f64e47affaf75549c10
l3lackheart <dgbao1340@gmail.com>
2018-08-01 22:16:48 +0700

    Add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { RestProvider } from '../providers/rest/rest';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RestProvider
   ]
 })
 export class AppModule {}
---
+++ src/providers/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+	fire: {
+		apiKey: "AIzaSyCL9O-P_rIDzMGtuEA6J7vsgS7Dvt_X7KE",
+        authDomain: "doowit-1c0e8.firebaseapp.com",
+        databaseURL: "https://doowit-1c0e8.firebaseio.com",
+        projectId: "doowit-1c0e8",
+        storageBucket: "doowit-1c0e8.appspot.com",
+        messagingSenderId: "34021985237"
+	}
+};
